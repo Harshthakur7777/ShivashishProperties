52cbcbd1ab0042d3bf98eb65977b0c2456b7cba9
Harshthakur7777 <thakurharsh08032003@gmail.com>
2024-09-19 16:09:01 +0530

    Updated files for new features/fixes

--- data/lands.js
+++ data/lands.js
@@ -1,11 +1,11 @@
 exports.array = [
     {
         index:1,
-     Name:"New Plot no 25 26 23 in SKYy solance",
+     Name:"Land with full water",
      SRB:"For Sale",
      rate:"2,50,00,000/begha",
      type:"land",
-     area:"Scheme No 140",
+     area:"jamniya",
      location :"jamniya khurd, near cricent water park, Indore" ,
      size:"6 Begha",
      image:"https://media.istockphoto.com/id/1500037690/photo/empty-room-with-white-wall-background-living-room-3d-rendering.jpg?s=612x612&w=0&k=20&c=qTp1GF4sGj_eB_pPOPvPZaQ6fcmL94jbyDaxaR2yzqM=",
